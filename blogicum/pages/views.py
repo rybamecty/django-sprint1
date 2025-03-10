3346a39fdfdb96238cd889bd83f17455d34f3583
Viktoriia Alekseenko <rybamecty003@gmail.com>
2025-03-10 14:59:54 +0100

    Добавил приложение blogicum и шаблоны

---
+++ blogicum/pages/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def about(request):
+    """View-функция для страницы 'О проекте'."""
+    return render(request, 'pages/about.html')
+
+def rules(request):
+    """View-функция для страницы 'Наши правила'."""
+    return render(request, 'pages/rules.html')
